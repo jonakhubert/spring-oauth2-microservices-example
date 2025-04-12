4b7316b325b16b037f3bcc88987e2dc9990cb2d7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2025-04-12 21:45:10 +0200

    Move ResourceController to /resource package

--- resource-server/src/main/java/com/some/resourceserver/resource/ResourceController.java
+++ resource-server/src/main/java/com/some/resourceserver/resource/ResourceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResourceController {
 
     @GetMapping(path = "/home")
-    public String home() {
+    public String greet() {
         return "Home";
     }
 }
