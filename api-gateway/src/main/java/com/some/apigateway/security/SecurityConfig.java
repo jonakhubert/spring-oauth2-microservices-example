9d6a93908f9260e2d33a1bfb834028febc5c3e0f
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2025-04-12 21:49:26 +0200

    Authenticate /api/resource routes

--- api-gateway/src/main/java/com/some/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/some/apigateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.some.apigateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -15,9 +16,9 @@ public class SecurityConfig {
         http
             .authorizeExchange(authorizeExchangeSpec ->
                 authorizeExchangeSpec
-                    .anyExchange()
-                    .permitAll()
-            );
+                    .pathMatchers("/api/resource/**").authenticated()
+                    .anyExchange().authenticated()
+            ).oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
